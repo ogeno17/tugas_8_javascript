dd7188c2fc2d24fbba83a6441d99abd3e565057e
hugoiman <hugogiman17@gmail.com>
2020-07-02 10:24:34 +0700

    Tugas 8 Javascript

---
+++ tugas8.js
@@ -0,0 +1,17 @@
+function objek() {
+  var virus = {
+    nama: "Corona 20",
+    warna: ["Merah", "Putih"],
+    tahun: 2020,
+  };
+
+  virus.nama = "Corona Virus Disease-19";
+  virus.bentuk = "Bulat";
+
+  console.log(virus);
+  console.log(virus.nama);
+  console.log(virus.warna);
+  console.log(virus.warna[1]);
+}
+
+objek();
